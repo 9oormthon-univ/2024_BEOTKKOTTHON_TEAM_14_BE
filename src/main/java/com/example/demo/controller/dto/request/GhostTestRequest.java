66b33950d42a87bf864a18677f38aaa081f20be7
woorimj <forestwr06@gmail.com>
2024-03-19 16:17:52 +0900

    Feat: 유령테스트 관련 service, controller, dto 추가

---
+++ src/main/java/com/example/demo/controller/dto/request/GhostTestRequest.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GhostTestRequest {
+    private int q1;
+    private int q2;
+    private int q3;
+    private int q4;
+
+}
