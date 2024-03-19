20a84d79107bf4e828ade0bc2536e7644373e94e
Jyebin <yebin0322@naver.com>
2024-03-19 15:15:21 +0900

    Refactor : 이름 변경

--- src/main/java/com/example/demo/repository/WillRepository.java
+++ src/main/java/com/example/demo/repository/WillRepository.java
@@ -1,11 +1,11 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.will.Will;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface WillRepository extends JpaRepository<Will, Long> {
-    Optional<Will> findBynameAndPassword(String name, String password);
-
-}
+//package com.example.demo.repository;
+//
+//import com.example.demo.domain.will.Will;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.Optional;
+//
+//public interface WillRepository extends JpaRepository<Will, Long> {
+//    Optional<Will> findBynameAndPassword(String name, String password);
+//
+//}
