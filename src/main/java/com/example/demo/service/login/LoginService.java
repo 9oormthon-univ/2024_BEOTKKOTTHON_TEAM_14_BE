d5b6eace3d79be18e865148e9833d6e3cc01734b
Jyebin <yebin0322@naver.com>
2024-03-19 09:15:20 +0900

    Feat : Login, 회원가입, Logout 기능 구현
    - LoginService 구현 완료

---
+++ src/main/java/com/example/demo/service/login/LoginService.java
@@ -0,0 +1,46 @@
+package com.example.demo.service.login;
+
+import com.example.demo.controller.dto.request.LoginRequestDto;
+import com.example.demo.domain.user.User;
+import com.example.demo.exception.ErrorCode;
+import com.example.demo.exception.model.CustomException;
+import com.example.demo.repository.LoginRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+    private final PasswordEncoder passwordEncoder;
+    private final LoginRepository loginRepository;
+    public void signIn(LoginRequestDto loginRequestDto) {
+        User user = loginRepository.findByEmail(loginRequestDto.getEmail()).orElseThrow(()-> new CustomException(ErrorCode.NOT_FOUND_USER_EXCEPTION, ErrorCode.NOT_FOUND_USER_EXCEPTION.getMessage()));
+        if(!passwordEncoder.matches(loginRequestDto.getPassword(), user.getPassword())){
+            throw new CustomException(ErrorCode.INVALID_PASSWORD_EXCEPTION, ErrorCode.INVALID_PASSWORD_EXCEPTION.getMessage());
+        }
+        System.out.println("로그인 성공");
+    }
+
+    public void register(LoginRequestDto loginRequestDto){
+        Optional<User> user = loginRepository.findByEmail(loginRequestDto.getEmail());
+        String encodedPassword = passwordEncoder.encode(loginRequestDto.getPassword());
+        if(user.isEmpty()){
+            User newUser = User.builder()
+                    .email(loginRequestDto.getEmail())
+                    .nickname(loginRequestDto.getNickName())
+                    .password(encodedPassword)
+                    .build();
+            loginRepository.save(newUser);
+            System.out.println("회원가입성공");
+        }
+
+    }
+    public void logout(){
+        SecurityContextHolder.clearContext();
+        System.out.println("로그아웃 성공");
+    }
+}
