74f6af5217fa3e826cfe6a71451ed8a2cf752a1a
Jyebin <yebin0322@naver.com>
2024-03-19 11:27:43 +0900

    Fix : not null 조건 추가
    - 비밀번호 정규식 dto로 이동
    - email 값에 notBlank 조건 추가

--- src/main/java/com/example/demo/domain/user/User.java
+++ src/main/java/com/example/demo/domain/user/User.java
@@ -3,6 +3,7 @@ package com.example.demo.domain.user;
 
 import com.example.demo.domain.BaseEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -13,14 +14,11 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@Table(name = "user")
 public class User extends BaseEntity {
+    @NotBlank(message = "이메일은 필수 입력 항목입니다.")
     @Column(unique = true)
     String email; //확장성을 위해 넣어둠
 
-    @Column(unique = true)
-    String nickname;
-
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$", message = "비밀번호는 최소 8자 이상이어야 하며, 최소 하나의 대문자, 소문자, 숫자, 특수 문자를 포함해야 합니다.")
+    @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
     String password;
 }
