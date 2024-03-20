06e846b24f5b64e1959eb6e4f924ef4bd0eda8a8
Jyebin <yebin0322@naver.com>
2024-03-21 01:34:45 +0900

    Test : 배포용 미구현 기능 주석처리

--- src/main/java/com/example/demo/repository/AnswerRepository.java
+++ src/main/java/com/example/demo/repository/AnswerRepository.java
@@ -1,12 +1,12 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.will.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-@Repository
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-    Optional<Answer> findByUserId(Long userId);
-}
+//package com.example.demo.repository;
+//
+//import com.example.demo.domain.will.Answer;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//
+//@Repository
+//public interface AnswerRepository extends JpaRepository<Answer, Long> {
+//    Optional<Answer> findByUserId(Long userId);
+//}
