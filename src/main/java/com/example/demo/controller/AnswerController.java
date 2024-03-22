9ff4bae65311ca90d26862dbf0d944b74d884f44
Jyebin <yebin0322@naver.com>
2024-03-23 02:07:02 +0900

    Feat : Answer 기능 구현
    - AnswerController 구현중

--- src/main/java/com/example/demo/controller/AnswerController.java
+++ src/main/java/com/example/demo/controller/AnswerController.java
@@ -1,4 +1,35 @@
-//package com.example.demo.controller;
-//
-//public class AnswerController {
-//}
+package com.example.demo.controller;
+
+import com.example.demo.common.dto.BaseResponse;
+import com.example.demo.controller.dto.request.AnswerRequestDto;
+import com.example.demo.exception.SuccessCode;
+import com.example.demo.service.will.AnswerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/answer")
+public class AnswerController {
+    private final AnswerService answerService;
+
+    @PostMapping("/create")
+    public BaseResponse<String> createAnswer(@RequestBody @Valid AnswerRequestDto answerRequestDto) {
+        answerService.createAnswer(answerRequestDto);
+        return BaseResponse.success(SuccessCode.CREATE_COMPLETE_SUCCESS, SuccessCode.CREATE_COMPLETE_SUCCESS.getMessage());
+    }
+
+    @DeleteMapping("/delete/{userId}")
+    public BaseResponse<String> deleteAnswer(@PathVariable Long userId) {
+        answerService.deleteAnswer(userId);
+        return BaseResponse.success(SuccessCode.DELETE_SUCCESS, SuccessCode.DELETE_SUCCESS.getMessage());
+    }
+
+    @GetMapping("/get/{userId}")
+    public BaseResponse<String> getAnswer(@PathVariable Long userId) {
+        answerService.getAnswer(userId);
+        return BaseResponse.success(SuccessCode.GET_SUCCESS, SuccessCode.GET_SUCCESS.getMessage());
+    }
+}
